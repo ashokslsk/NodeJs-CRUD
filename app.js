ba09792cf956054b3478d2ed4ec22257a34463ec
ashok.kumar <ashok.kumar@dunstconsulting.in>
2016-12-27 15:15:39 +0530

    Added router to fetch the mongodbdata collections

--- app.js
+++ app.js
@@ -3,18 +3,33 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Book = require('./Book.model');
-
+//Listning port
+var port = 8080;
 //Variable for database
 var db = 'mongodb://localhost/bookstore';
 
 
 mongoose.connect(db);
 
-//Listning port
-var port = 8080;
+app.get('/', function(req,res){
+   res.send('happy to learn nodeJS');
+});
+
+app.get('/books', function(req,res){
+	console.log('getting all books');
+	Book.find({})
+	.exec(function(err, books){
+		if(err){
+			res.send('error has occured');
+		}else {
+			console.log('getting all books ->' + books);
+			res.json(books);
+		}
+	});
+});
 
 app.listen(port, function(){
-	console.log('App listning on port' + port);
+	console.log('App listning on port :' + port);
 });
 
 
